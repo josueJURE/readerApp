4dcb2a579e6c1c44fd16577ef89461097445d4da
josueJURE <josue.jure@gmail.com>
2022-02-09 16:47:56 +0000

    use fat arrow function in eventListener on read e lement

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const read = document.getElementById("read");
 const pause = document.getElementById("pause");
 const stop = document.getElementById("stop");
 const speed = document.getElementById("speed");
-const text = document.getElementById("text")
+const text = document.getElementById("text");
 
 
-read.addEventListener("click", function() {
+read.addEventListener("click", () => {
   readText(text.value)
 });
 
+
 var utterThis = new SpeechSynthesisUtterance();
 
-console.log(utterThis)
+
+
+function readText(text) {
+  utterThis.text = text;
+  utterThis.rate = speed.value || 1;
+  text.disabled = true;
+  speechSynthesis.speak(utterThis)
+}
